b5a3bbdcc89ce6c8f017df1cadbf65a068367ec0
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-01-08 22:00:04 +0800

    show result message when user wins

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const result_div = document.querySelector('.result');
+const result_p = document.querySelector('.result > p');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
@@ -22,6 +22,7 @@ function win(userChoice, computerChoice) {
   userScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = userChoice + ' beats ' + computerChoice + '. You win!';
 }
 
 // UserChoice vs ComputerChoice Function
